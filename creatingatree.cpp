7f0940ea30e45a6f57f82162f58972f0f9313f7e
user.name like : Insiya <zaidiinsiya83@gmail.com>
2025-10-08 00:07:45 +0530

    .

--- creatingatree.cpp
+++ creatingatree.cpp
@@ -1,35 +1,31 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-
 class Node{
-public:
-int data ;
-Node*right;
-Node*left;
-
-Node(int val){
-    data=val;
-    right=left=NULL;
-}
+    public:
+    int data;
+    Node*right;
+     Node*left;
+     Node(int val){
+        data = val;
+        right=left=NULL;
+     }
 };
-static int  idx = -1;
-Node*buildtree(vector<int>preorder){
-idx++;
+static int idx = -1;
+  Node* creatingtree(vector<int>preorder){
+    idx++;
 if(preorder[idx]==-1){
-    return NULL;
+return NULL;
 }
-Node*root = new Node(preorder[idx]);
-root->left = buildtree(preorder);
-root->right = buildtree(preorder);
-return root;
+Node* root = new Node(preorder[idx]);
+ root->left = creatingtree(preorder);
+  root->right = creatingtree(preorder);
+  return root;
 }
-
-
 int main(){
-    vector<int>preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
-  Node*root =   buildtree(preorder);
-  cout<<root->data << endl;
-    cout<<root->left->data << endl;
-    cout<<root->right->data << endl;
+    vector<int>preorder={1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+ Node* root = creatingtree(preorder);
+ cout<< root->data<<endl;
+ cout<< root->left->data<<endl;
+ cout<< root->right->data<<endl;
 }
