fac5ccf4a27c4c9eef17e54f16bfe4b70d2a8625
user.name like : Insiya <zaidiinsiya83@gmail.com>
2025-10-11 15:56:45 +0530

    using ip or is

--- deletenodebst.cpp
+++ deletenodebst.cpp
@@ -12,83 +12,95 @@ Node(int val){
 }
 };
 
-Node* insert(Node*root , int val){
-    if (root==NULL){
+ Node* insert( Node*root , int val){
+    if(root==NULL){
         return new Node(val);
     }
-    if(val<root->data){
-      root->left  = insert(root->left , val);  // insert jb new node bnaiga aur uski call  yha se gai toh new node return bhi yhi hogi aur assign bhi hogi root->left 
-    }
-    else{
-        root->right = insert(root->right , val);
-    }
-    return root ;  // .. yha se eventually root 3 hi return hogi baar baar 
-}   
+if(val<root->data){
+   root->left =  insert(root->left , val); // yeh tb ke liye h jb root ka left null nhi h kuch node present h toh vo phir se further root ke left m 
+} //  search krta h kbha next root ka left null h taaki h  new root ko insert krsku 
+if(val>root->data){
+    root->right = insert(root->right , val);
+}
+return root;  // jb insert hojaiga node toh last m  new root bhejdena kyu ki kuch changes toh hue honge na tree m add hone ke baad 
+ }
 
-Node* buildbst( vector<int>arr){
-   Node* root = NULL;
-   for(int val:arr){
-   root =  insert(root , val);
-   } 
-   return root;
-}
+ Node* buildbst( vector<int>arr){
+ Node* root = NULL;
+for(int val: arr){
+root = insert(root , val);
 
-Node*getis(Node*root){
-    while(  root!=NULL &&root->left!=NULL ){
-        root=root->left;
-    }
-    return root;
+} 
+return root ;
 }
- Node* deletenode(Node*root , int key){
-   if(root==NULL){
-    return NULL;
-   }
-   
-    if(key<root->data){
-            root->left = deletenode(root->left , key); // yeh root ka new left part dega toh usko phir root se join krvadiya
-    }
-  else  if(root->data<key){
-          root->right = deletenode(root->right , key);
-    }
-    else {   // yaani hme root mil gya jisko delte krna h 
-if(root->right==NULL){   
-Node*temp = root->left;
-delete root;              // starting ke 2 cases m hmne no child , 1 child ko handle krliya h 
-return temp;
-}
-else if(root->left==NULL){
-    Node*temp= root->right;
-    delete root;
-    return temp;
-}
-else{   // 2 child .. find inorder successor -- right subtree .. leftmost node  .. aur IS ka hmesha 0 ya phir 1 hi child hoga 2 kbhi nhi 
-Node*IS = getis(root->right);
-
-root->data = IS->data;
- root->right = deletenode(root->right ,IS->data );  // kyu ki IS hmesha right m exist krta h isliye , aur hm  root ke right se hi delete krege 
-
-
-     
-}
-    }
-    return root;
-}
-void printinoder( Node*root){
+ void printinorder( Node* root){
     if(root==NULL){
         return;
     }
-    printinoder(root->left);
+    printinorder(root->left);
     cout<<root->data<<" ";
-     printinoder(root->right);
+    printinorder(root->right);
 }
+
+// Node* getis(Node*root){
+  //while( root!=NULL && root->left!=NULL){
+  //  root=root->left;
+//}
+ // return root ;
+ // }
+
+ Node* getip(Node*root){
+    while(root!=NULL && root->right!=NULL){
+        root=root->right;
+    }
+    return root ; 
+ }
+
+
+Node* deletenode( Node* root , int key){
+    if(root==NULL){
+        return NULL;
+    }
+    if(root->data>key){
+       root->left  =    deletenode(root->left , key); 
+    }
+    else if(root->data<key){
+        root->right = deletenode(root->right , key );
+    }
+    else {  // yaani root mil gaiii 
+if(root->left ==NULL){
+Node*temp = root->right;
+delete root ;
+return temp;
+}
+ else if(root->right ==NULL){
+Node*temp = root->left;
+delete root ;
+return temp;
+}
+else{ 
+
+    Node*IP = getip (root->left);
+    root->data= IP->data;
+    root->left = deletenode(root->left , IP->data);
+//Node* IS = getis(root->right);
+//root->data= IS->data;
+ //root->right = deletenode(root->right , IS->data);
+}
+    }
+
+return root;
+}
+
 int main(){
-    vector<int>arr ={3,2,1,5,6,4};
+   // vector<int>arr ={3,2,1,5,6,4};
+   vector<int>arr ={8, 3, 10, 1, 6, 4, 7, 14, 13, 5};
    Node*root =  buildbst(arr);
    cout<<" before deletion"<<endl;
-   printinoder(root);
-   deletenode(root , 5);
+   printinorder(root);
+   deletenode(root , 6 );    // BHAIII HM IS AUR IP DONO  M SE  KISI EK KI HELP SE  DELETE KRSKTE H NODE ... 
    cout<<endl;
-     cout<<" after  deletion"<<endl;
-   printinoder(root);
+      cout<<" after  deletion"<<endl;
+    printinorder(root);
   
 }
