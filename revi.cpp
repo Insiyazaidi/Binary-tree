fd9f278d2dd8e31371eb429ea5005f614e1b6c4e
user.name like : Insiya <zaidiinsiya83@gmail.com>
2025-12-27 21:16:08 +0530

    27 jan

--- revi.cpp
+++ revi.cpp
@@ -1,91 +1,108 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<map>
 using namespace std;
-  class Node {
+class Node{
     public:
     int data ;
-    Node*left ;
-    Node*right ;
+    Node*left;
+    Node*right;
     Node(int value){
-        left = right = NULL;
         data = value ;
+        left = right = NULL;
     }
-  };
-  static int idx = -1;
-Node* creatingtree(vector<int>preorder){
+};
+static int idx = -1;
+ Node * creatingtree(vector<int>v){
 idx++;
-if(preorder[idx]==-1){
+if(v[idx]==-1){
     return NULL;
 }
-Node * root = new Node (preorder[idx]);
- root->left = creatingtree(preorder);
-  root->right = creatingtree(preorder);
+Node* root = new Node(v[idx]);
+root->left = creatingtree(v);
+root->right = creatingtree(v);
 return root ; 
- } 
-
- void levelorder(Node*root){
+}
+  void levelorder(Node* root){
 queue<Node*>q;
 q.push(root);
 q.push(NULL);
 while(q.size()>0){
     Node*curr = q.front();
-       q.pop();
+    q.pop();
     if(curr==NULL){
         if(!q.empty()){
- cout<< endl;
- q.push(NULL);
- continue;
+    q.push(NULL);
+    continue;
         }
         else{
             break;
         }
-       
     }
- 
-    cout << curr->data  << " ";
+    cout << curr->data<< " " ;
     if(curr->left!=NULL){
         q.push(curr->left);
+
     }
-       if(curr->right!=NULL){
-        q.push(curr->right);
+      if(curr->right!=NULL){
+        q.push(curr->right);  
     }
+}
 }
 
+topview(Node*root){
+    queue<pair<int , Node*>>q;
+    map<int, Node*>m;
+    q.push({0,root});
+    while(q.size()>0){
+        auto curr = q.front();
+        q.pop();
+        Node* node = curr.second;
+        int hd = curr.first;
+        
+m[hd] = node ;
+if(node->left){
+    q.push({hd-1 , node->left});
 }
-int height (Node*root){
-if(root==NULL){
-    return 0;
+if(node->right){
+    q.push({ hd+1 , node->right});
+}
+    }
+    for(auto it:m){
+        cout << it.second->data<< " ";
+    }
+}
+  void klevel(Node*root , int k ){
+queue<pair<Node* , int>>q;
+q.push({root , k});
+while(q.size()>0){
+
+ auto curr = q.front();
+ q.pop();
+  Node* node = curr.first;
+ int level = curr.second;
+  if(level==1){
+cout<<node->data << "  ";
+continue;
+ }
+
+ if(node->left!=NULL){
+    q.push({node->left  , level-1});
+ }
+ if(node->right!=NULL){
+    q.push({node->right  , level-1});
+ }
 }
 
-  int leftheight =   height(root->left);
- int rightheight =    height(root->right);
- return    max(leftheight , rightheight)+1; 
+
+
 }
 
-int countnode(Node*root){
-    if(root==NULL){
-        return 0;
-    }
-   int leftcount =  countnode(root->left);
-int rightcount =     countnode(root->right);
-return leftcount+rightcount+1;
-}
-int sumnode(Node*root){
-    if(root==NULL){
-return 0;
-    }
-   int leftsum  = sumnode(root->left);
-  int rightsum     = sumnode(root->right);
-  return leftsum+rightsum+root->data ; 
-}
 int main(){
-       vector<int> preorder = {1,2,4,8, -1, -1, 9, -1, -1, 5, 10, -1, -1,-1, 3, 6, -1, -1, 7, -1, -1};
-
-//vector<int>preorder= {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
-Node*root =  creatingtree(preorder);
-// levelorder(root);
-//  cout << height(root);
-// cout << countnode(root);
- cout << sumnode(root);
+   vector<int> v = {1,2, 4, -1, 7, -1, -1, 5, 8, -1, -1, -1, 3, -1,  6, 9, -1, -1, 10, -1, -1};
+   Node*root =  creatingtree(v);
+//levelorder(root);
+ //topview(root);
+ klevel(root , 2);
 }
