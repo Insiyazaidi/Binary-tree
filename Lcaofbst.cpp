7ec7407802e38fe32469cb0930fe80ac08c142e2
user.name like : Insiya <zaidiinsiya83@gmail.com>
2026-01-16 19:06:48 +0530

    16 jan

--- Lcaofbst.cpp
+++ Lcaofbst.cpp
@@ -13,3 +13,20 @@ public:
             return root;
         }
     }
+    class Solution {
+public:
+bool validate (TreeNode* root , long min , long max){
+    if(root==NULL){
+        return true ; 
+    }
+    if(root->val<=min || root->val>=max){  // agr range se bhr chali gai 
+        return false ; 
+    }
+    bool leftsubtree = validate(root->left , min , root->val);
+    bool rightsubtree =  validate(root->right , root->val , max);
+    return leftsubtree && rightsubtree;
+}
+    bool isValidBST(TreeNode* root) {
+      return validate(root , LONG_MIN , LONG_MAX );  
+    }
+};
