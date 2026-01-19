2fec2636d1bb6123b009441ad1568e9b4a39e395
user.name like : Insiya <zaidiinsiya83@gmail.com>
2026-01-19 19:09:10 +0530

    19 jan

--- revi.cpp
+++ revi.cpp
@@ -1,57 +1,25 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-class Node{
-    public:
-    int data ; 
-    Node* right;
-    Node* left;
-    Node(int value){
-        data = value;
-        right=left=NULL;
+class Solution {
+public:
+int ans = INT_MAX;
+TreeNode*prev = NULL;
+    int minDiffInBST(TreeNode* root) {
+        if(root==NULL){
+            return INT_MAX;
+        }
+        if(root->left !=NULL){
+         minDiffInBST(root->left);
+        
+        }
+        if(prev!=NULL){
+  ans = min(ans , root->val-prev->val);
+        }
+      
+        prev=root;
+          if(root->right){
+          minDiffInBST(root->right);
+            
+        }
+        return ans ;
+
     }
 };
-Node* insert(Node* root , int value){
-if(root==NULL){
-    return new Node(value);
-}
-if(root->data>value){  //4 agr root h aur hmari value 2 hai ... 
-  root->left = insert(root->left , value);
-}
-else{
-    root->right = insert(root->right , value);
-}
-return root ;
-}
-
-Node* buildbst(vector<int>arr){
-    Node* root = NULL;
-    for(int value : arr){
-  root = insert(root , value);
-    }
-
-    return root;
-}
- bool search(Node* root , int key){
-if(root==NULL){
-    return 0;
-}
-if(root->data==key){
-return 1;
-}
-if(root->data>key){
-    search(root->left , key);
-}
-else{
-    search(root->right , key);
-}
-}
-
-
-int main(){   
-    vector<int>arr = {3,2,1,5,6,4};
-    Node* root = buildbst(arr);
-    cout<< endl;
-  cout <<  search(root , 9);
-
-}
